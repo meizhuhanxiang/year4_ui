ead8b6154b5ff2c0fbee4b4791a86975b4002a31
Guo Guangchuan <ggc0402@qq.com>
2016-09-01 13:45:03 +0800

    update

--- src/js/valid.js
+++ src/js/valid.js
@@ -1,96 +1,96 @@
 Function.prototype.after = function(afterfn) {
-    var self = this;
-    return function() {
-      var ret = self.apply(this, arguments);
-      afterfn.call(this, ret);
-      return ret;
+        var self = this;
+        return function() {
+            var ret = self.apply(this, arguments);
+            afterfn.call(this, ret);
+            return ret;
+        }
     }
-  }
-  // 策略对象
+    // 策略对象
 var strategies = {
-    isNonEmpty: function(value, errMsg) {
-      if (value == "") {
-        return errMsg
-      }
-    },
-    minLength: function(value, len, errMsg) {
-      if (value.length < len) {
-        return errMsg;
-      }
-    },
-    length: function(value, len, errMsg) {
-      if (value.length != len) {
-        return errMsg;
-      }
-    },
-    isMobile: function(value, errMsg) {
-      if (!/1[3|5|9][0-9]{9}/.test(value)) {
-        return errMsg;
-      }
+        isNonEmpty: function(value, errMsg) {
+            if (value == "") {
+                return errMsg
+            }
+        },
+        minLength: function(value, len, errMsg) {
+            if (value.length < len) {
+                return errMsg;
+            }
+        },
+        length: function(value, len, errMsg) {
+            if (value.length != len) {
+                return errMsg;
+            }
+        },
+        isMobile: function(value, errMsg) {
+            if (!/1[3|5|9][0-9]{9}/.test(value)) {
+                return errMsg;
+            }
+        }
     }
-  }
-  //Validator 对象
-  /*
-  add 定义规则 返回一个规则执行函数的数组
-  start 执行规则 返回一个错误信息的结果
-  都将具体的逻辑委托给了strategies 对象
-  符合了单一职责和开放封闭原则
-  开放算法的扩展
-  封闭了Validator的修改
-  */
+    //Validator 对象
+    /*
+    add 定义规则 返回一个规则执行函数的数组
+    start 执行规则 返回一个错误信息的结果
+    都将具体的逻辑委托给了strategies 对象
+    符合了单一职责和开放封闭原则
+    开放算法的扩展
+    封闭了Validator的修改
+    */
 var Validator = function() {
-    this.cache = [];
-  }
-  // 为了做验证拼参数
+        this.cache = [];
+    }
+    // 为了做验证拼参数
 Validator.prototype.add = function(dom, rules) {
-    for (var i = 0, rule; rule = rules[i++];) {
-      // 闭包利用的非常合理 把每个规则都封装到闭包里面真是神来之笔
-      // 验证的函数是相对于规则来说的 其中公有的部分是对应的dom
-      (function(rule) {
-        // 私有的部分是规则内部的解析参数
-        var strategArr = rule.stratege.split(":");
-        var errMsg = rule.errMsg;
-        var result;
-        var ifInit = true;
-        var stra;
-        // 将验证的函数放到cache里面
-        this.cache.push(function() {
-          // 组装参数列表 这里的function都是闭包 里面的变量都不会被销毁
-          if (ifInit) {
-            stra = strategArr.shift();
-            strategArr.push(errMsg);
-            ifInit = false;
-          }
-          strategArr.unshift(dom.value);
-          // console.log(stra, strategArr)
-          return result = strategies[stra].apply(dom, strategArr);
+        for (var i = 0, rule; rule = rules[i++];) {
+            // 闭包利用的非常合理 把每个规则都封装到闭包里面真是神来之笔
+            // 验证的函数是相对于规则来说的 其中公有的部分是对应的dom
+            (function(rule) {
+                // 私有的部分是规则内部的解析参数
+                var strategArr = rule.stratege.split(":");
+                var errMsg = rule.errMsg;
+                var result;
+                var ifInit = true;
+                var stra;
+                // 将验证的函数放到cache里面
+                this.cache.push(function() {
+                    // 组装参数列表 这里的function都是闭包 里面的变量都不会被销毁
+                    if (ifInit) {
+                        stra = strategArr.shift();
+                        strategArr.push(errMsg);
+                        ifInit = false;
+                    }
+                    strategArr.unshift(dom.value);
+                    // console.log(stra, strategArr)
+                    return result = strategies[stra].apply(dom, strategArr);
 
-        }.after(function(result) {
-          console.log(errMsg, result)
-          if (result == undefined) {
-            dom.nextSibling.innerHTML = "";
-          } else {
-            dom.nextSibling.innerHTML = result;
-          }
-          strategArr.shift()
-        }))
-      }.bind(this))(rule)
+                }.after(function(result) {
+                    console.log(errMsg, result)
+                    if (result == undefined) {
+                        dom.nextSibling.innerHTML = "";
+                    } else {
+                        dom.nextSibling.innerHTML = result;
+                    }
+                    strategArr.shift()
+                }))
+            }.bind(this))(rule)
+        }
     }
-  }
-  // 验证开始
+    // 验证开始
 Validator.prototype.start = function() {
-    var ifValidate = false;
-    for (var j = 0, validaFuncc; validaFuncc = this.cache[j++];) {
-      var err = validaFuncc();
-      // console.log(err)
-      if (err) {
-        // ifValidate = true;
-        return err;
-      }
+        var ifValidate = false;
+        for (var j = 0, validaFuncc; validaFuncc = this.cache[j++];) {
+            var err = validaFuncc();
+            // console.log(err)
+            if (err) {
+                // ifValidate = true;
+                return err;
+            }
+        }
+        // return ifValidate;
     }
-    // return ifValidate;
-  }
-  // 客户端调用
+    // 客户端调用
 var registerForm = document.getElementById('registerForm');
 var validator = new Validator();
 var userName = document.getElementById('userName');
@@ -104,16 +104,16 @@ var submit = document.getElementById('submit');
 //   errMsg: '用户名长度不能小于10 位'
 // }]);
 validator.add(userName, [{
-  stratege: 'isNonEmpty',
-  errMsg: '用户名不能为空'
+    stratege: 'isNonEmpty',
+    errMsg: '用户名不能为空'
 }]);
 
 validator.add(phone, [{
-  stratege: 'isNonEmpty',
-  errMsg: '手机号不能为空'
+    stratege: 'isNonEmpty',
+    errMsg: '手机号不能为空'
 }, {
-  stratege: 'length:11',
-  errMsg: '手机号为11位'
+    stratege: 'length:11',
+    errMsg: '手机号为11位'
 }]);
 
 
@@ -127,14 +127,29 @@ validator.add(phone, [{
 // }]);
 
 var validataFunc = function() {
-  var errorMsg = validator.start();
-  return errorMsg;
+    var errorMsg = validator.start();
+    return errorMsg;
 }
 submit.onclick = function() {
-  var errorMsg = validataFunc();
-  if (errorMsg) {
-    return false;
-  }
+    var errorMsg = validataFunc();
+    if (errorMsg) {
+        return false;
+    } else {
+        reqwest({
+            url: '/order/signup',
+            method: 'post',
+            data: {
+                "kind_id": [1,2],
+                "purchase_id": "test",
+                "phone": "1342461052",
+                "name": "aaa",
+                "wechat_no": "asdad"
+            },
+            success: function(resp) {
+                console.log("请求成功")
+            }
+        })
+    }
 };
 // 策略模式
 // 组合 不同类之间的组合
--- static/js/main.js
+++ static/js/main.js
@@ -5,98 +5,98 @@
   */
 !function(e,t,n){typeof module!="undefined"&&module.exports?module.exports=n():typeof define=="function"&&define.amd?define(n):t[e]=n()}("reqwest",this,function(){function succeed(e){var t=protocolRe.exec(e.url);return t=t&&t[1]||context.location.protocol,httpsRe.test(t)?twoHundo.test(e.request.status):!!e.request.response}function handleReadyState(e,t,n){return function(){if(e._aborted)return n(e.request);if(e._timedOut)return n(e.request,"Request is aborted: timeout");e.request&&e.request[readyState]==4&&(e.request.onreadystatechange=noop,succeed(e)?t(e.request):n(e.request))}}function setHeaders(e,t){var n=t.headers||{},r;n.Accept=n.Accept||defaultHeaders.accept[t.type]||defaultHeaders.accept["*"];var i=typeof FormData!="undefined"&&t.data instanceof FormData;!t.crossOrigin&&!n[requestedWith]&&(n[requestedWith]=defaultHeaders.requestedWith),!n[contentType]&&!i&&(n[contentType]=t.contentType||defaultHeaders.contentType);for(r in n)n.hasOwnProperty(r)&&"setRequestHeader"in e&&e.setRequestHeader(r,n[r])}function setCredentials(e,t){typeof t.withCredentials!="undefined"&&typeof e.withCredentials!="undefined"&&(e.withCredentials=!!t.withCredentials)}function generalCallback(e){lastValue=e}function urlappend(e,t){return e+(/\?/.test(e)?"&":"?")+t}function handleJsonp(e,t,n,r){var i=uniqid++,s=e.jsonpCallback||"callback",o=e.jsonpCallbackName||reqwest.getcallbackPrefix(i),u=new RegExp("((^|\\?|&)"+s+")=([^&]+)"),a=r.match(u),f=doc.createElement("script"),l=0,c=navigator.userAgent.indexOf("MSIE 10.0")!==-1;return a?a[3]==="?"?r=r.replace(u,"$1="+o):o=a[3]:r=urlappend(r,s+"="+o),context[o]=generalCallback,f.type="text/javascript",f.src=r,f.async=!0,typeof f.onreadystatechange!="undefined"&&!c&&(f.htmlFor=f.id="_reqwest_"+i),f.onload=f.onreadystatechange=function(){if(f[readyState]&&f[readyState]!=="complete"&&f[readyState]!=="loaded"||l)return!1;f.onload=f.onreadystatechange=null,f.onclick&&f.onclick(),t(lastValue),lastValue=undefined,head.removeChild(f),l=1},head.appendChild(f),{abort:function(){f.onload=f.onreadystatechange=null,n({},"Request is aborted: timeout",{}),lastValue=undefined,head.removeChild(f),l=1}}}function getRequest(e,t){var n=this.o,r=(n.method||"GET").toUpperCase(),i=typeof n=="string"?n:n.url,s=n.processData!==!1&&n.data&&typeof n.data!="string"?reqwest.toQueryString(n.data):n.data||null,o,u=!1;return(n["type"]=="jsonp"||r=="GET")&&s&&(i=urlappend(i,s),s=null),n["type"]=="jsonp"?handleJsonp(n,e,t,i):(o=n.xhr&&n.xhr(n)||xhr(n),o.open(r,i,n.async===!1?!1:!0),setHeaders(o,n),setCredentials(o,n),context[xDomainRequest]&&o instanceof context[xDomainRequest]?(o.onload=e,o.onerror=t,o.onprogress=function(){},u=!0):o.onreadystatechange=handleReadyState(this,e,t),n.before&&n.before(o),u?setTimeout(function(){o.send(s)},200):o.send(s),o)}function Reqwest(e,t){this.o=e,this.fn=t,init.apply(this,arguments)}function setType(e){if(e===null)return undefined;if(e.match("json"))return"json";if(e.match("javascript"))return"js";if(e.match("text"))return"html";if(e.match("xml"))return"xml"}function init(o,fn){function complete(e){o.timeout&&clearTimeout(self.timeout),self.timeout=null;while(self._completeHandlers.length>0)self._completeHandlers.shift()(e)}function success(resp){var type=o.type||resp&&setType(resp.getResponseHeader("Content-Type"));resp=type!=="jsonp"?self.request:resp;var filteredResponse=globalSetupOptions.dataFilter(resp.responseText,type),r=filteredResponse;try{resp.responseText=r}catch(e){}if(r)switch(type){case"json":try{resp=context.JSON?context.JSON.parse(r):eval("("+r+")")}catch(err){return error(resp,"Could not parse JSON in response",err)}break;case"js":resp=eval(r);break;case"html":resp=r;break;case"xml":resp=resp.responseXML&&resp.responseXML.parseError&&resp.responseXML.parseError.errorCode&&resp.responseXML.parseError.reason?null:resp.responseXML}self._responseArgs.resp=resp,self._fulfilled=!0,fn(resp),self._successHandler(resp);while(self._fulfillmentHandlers.length>0)resp=self._fulfillmentHandlers.shift()(resp);complete(resp)}function timedOut(){self._timedOut=!0,self.request.abort()}function error(e,t,n){e=self.request,self._responseArgs.resp=e,self._responseArgs.msg=t,self._responseArgs.t=n,self._erred=!0;while(self._errorHandlers.length>0)self._errorHandlers.shift()(e,t,n);complete(e)}this.url=typeof o=="string"?o:o.url,this.timeout=null,this._fulfilled=!1,this._successHandler=function(){},this._fulfillmentHandlers=[],this._errorHandlers=[],this._completeHandlers=[],this._erred=!1,this._responseArgs={};var self=this;fn=fn||function(){},o.timeout&&(this.timeout=setTimeout(function(){timedOut()},o.timeout)),o.success&&(this._successHandler=function(){o.success.apply(o,arguments)}),o.error&&this._errorHandlers.push(function(){o.error.apply(o,arguments)}),o.complete&&this._completeHandlers.push(function(){o.complete.apply(o,arguments)}),this.request=getRequest.call(this,success,error)}function reqwest(e,t){return new Reqwest(e,t)}function normalize(e){return e?e.replace(/\r?\n/g,"\r\n"):""}function serial(e,t){var n=e.name,r=e.tagName.toLowerCase(),i=function(e){e&&!e.disabled&&t(n,normalize(e.attributes.value&&e.attributes.value.specified?e.value:e.text))},s,o,u,a;if(e.disabled||!n)return;switch(r){case"input":/reset|button|image|file/i.test(e.type)||(s=/checkbox/i.test(e.type),o=/radio/i.test(e.type),u=e.value,(!s&&!o||e.checked)&&t(n,normalize(s&&u===""?"on":u)));break;case"textarea":t(n,normalize(e.value));break;case"select":if(e.type.toLowerCase()==="select-one")i(e.selectedIndex>=0?e.options[e.selectedIndex]:null);else for(a=0;e.length&&a<e.length;a++)e.options[a].selected&&i(e.options[a])}}function eachFormElement(){var e=this,t,n,r=function(t,n){var r,i,s;for(r=0;r<n.length;r++){s=t[byTag](n[r]);for(i=0;i<s.length;i++)serial(s[i],e)}};for(n=0;n<arguments.length;n++)t=arguments[n],/input|select|textarea/i.test(t.tagName)&&serial(t,e),r(t,["input","select","textarea"])}function serializeQueryString(){return reqwest.toQueryString(reqwest.serializeArray.apply(null,arguments))}function serializeHash(){var e={};return eachFormElement.apply(function(t,n){t in e?(e[t]&&!isArray(e[t])&&(e[t]=[e[t]]),e[t].push(n)):e[t]=n},arguments),e}function buildParams(e,t,n,r){var i,s,o,u=/\[\]$/;if(isArray(t))for(s=0;t&&s<t.length;s++)o=t[s],n||u.test(e)?r(e,o):buildParams(e+"["+(typeof o=="object"?s:"")+"]",o,n,r);else if(t&&t.toString()==="[object Object]")for(i in t)buildParams(e+"["+i+"]",t[i],n,r);else r(e,t)}var context=this;if("window"in context)var doc=document,byTag="getElementsByTagName",head=doc[byTag]("head")[0];else{var XHR2;try{XHR2=require("xhr2")}catch(ex){throw new Error("Peer dependency `xhr2` required! Please npm install xhr2")}}var httpsRe=/^http/,protocolRe=/(^\w+):\/\//,twoHundo=/^(20\d|1223)$/,readyState="readyState",contentType="Content-Type",requestedWith="X-Requested-With",uniqid=0,callbackPrefix="reqwest_"+ +(new Date),lastValue,xmlHttpRequest="XMLHttpRequest",xDomainRequest="XDomainRequest",noop=function(){},isArray=typeof Array.isArray=="function"?Array.isArray:function(e){return e instanceof Array},defaultHeaders={contentType:"application/x-www-form-urlencoded",requestedWith:xmlHttpRequest,accept:{"*":"text/javascript, text/html, application/xml, text/xml, */*",xml:"application/xml, text/xml",html:"text/html",text:"text/plain",json:"application/json, text/javascript",js:"application/javascript, text/javascript"}},xhr=function(e){if(e.crossOrigin===!0){var t=context[xmlHttpRequest]?new XMLHttpRequest:null;if(t&&"withCredentials"in t)return t;if(context[xDomainRequest])return new XDomainRequest;throw new Error("Browser does not support cross-origin requests")}return context[xmlHttpRequest]?new XMLHttpRequest:XHR2?new XHR2:new ActiveXObject("Microsoft.XMLHTTP")},globalSetupOptions={dataFilter:function(e){return e}};return Reqwest.prototype={abort:function(){this._aborted=!0,this.request.abort()},retry:function(){init.call(this,this.o,this.fn)},then:function(e,t){return e=e||function(){},t=t||function(){},this._fulfilled?this._responseArgs.resp=e(this._responseArgs.resp):this._erred?t(this._responseArgs.resp,this._responseArgs.msg,this._responseArgs.t):(this._fulfillmentHandlers.push(e),this._errorHandlers.push(t)),this},always:function(e){return this._fulfilled||this._erred?e(this._responseArgs.resp):this._completeHandlers.push(e),this},fail:function(e){return this._erred?e(this._responseArgs.resp,this._responseArgs.msg,this._responseArgs.t):this._errorHandlers.push(e),this},"catch":function(e){return this.fail(e)}},reqwest.serializeArray=function(){var e=[];return eachFormElement.apply(function(t,n){e.push({name:t,value:n})},arguments),e},reqwest.serialize=function(){if(arguments.length===0)return"";var e,t,n=Array.prototype.slice.call(arguments,0);return e=n.pop(),e&&e.nodeType&&n.push(e)&&(e=null),e&&(e=e.type),e=="map"?t=serializeHash:e=="array"?t=reqwest.serializeArray:t=serializeQueryString,t.apply(null,n)},reqwest.toQueryString=function(e,t){var n,r,i=t||!1,s=[],o=encodeURIComponent,u=function(e,t){t="function"==typeof t?t():t==null?"":t,s[s.length]=o(e)+"="+o(t)};if(isArray(e))for(r=0;e&&r<e.length;r++)u(e[r].name,e[r].value);else for(n in e)e.hasOwnProperty(n)&&buildParams(n,e[n],i,u);return s.join("&").replace(/%20/g,"+")},reqwest.getcallbackPrefix=function(){return callbackPrefix},reqwest.compat=function(e,t){return e&&(e.type&&(e.method=e.type)&&delete e.type,e.dataType&&(e.type=e.dataType),e.jsonpCallback&&(e.jsonpCallbackName=e.jsonpCallback)&&delete e.jsonpCallback,e.jsonp&&(e.jsonpCallback=e.jsonp)),new Reqwest(e,t)},reqwest.ajaxSetup=function(e){e=e||{};for(var t in e)globalSetupOptions[t]=e[t]},reqwest})
 Function.prototype.after = function(afterfn) {
-    var self = this;
-    return function() {
-      var ret = self.apply(this, arguments);
-      afterfn.call(this, ret);
-      return ret;
+        var self = this;
+        return function() {
+            var ret = self.apply(this, arguments);
+            afterfn.call(this, ret);
+            return ret;
+        }
     }
-  }
-  // 策略对象
+    // 策略对象
 var strategies = {
-    isNonEmpty: function(value, errMsg) {
-      if (value == "") {
-        return errMsg
-      }
-    },
-    minLength: function(value, len, errMsg) {
-      if (value.length < len) {
-        return errMsg;
-      }
-    },
-    length: function(value, len, errMsg) {
-      if (value.length != len) {
-        return errMsg;
-      }
-    },
-    isMobile: function(value, errMsg) {
-      if (!/1[3|5|9][0-9]{9}/.test(value)) {
-        return errMsg;
-      }
+        isNonEmpty: function(value, errMsg) {
+            if (value == "") {
+                return errMsg
+            }
+        },
+        minLength: function(value, len, errMsg) {
+            if (value.length < len) {
+                return errMsg;
+            }
+        },
+        length: function(value, len, errMsg) {
+            if (value.length != len) {
+                return errMsg;
+            }
+        },
+        isMobile: function(value, errMsg) {
+            if (!/1[3|5|9][0-9]{9}/.test(value)) {
+                return errMsg;
+            }
+        }
     }
-  }
-  //Validator 对象
-  /*
-  add 定义规则 返回一个规则执行函数的数组
-  start 执行规则 返回一个错误信息的结果
-  都将具体的逻辑委托给了strategies 对象
-  符合了单一职责和开放封闭原则
-  开放算法的扩展
-  封闭了Validator的修改
-  */
+    //Validator 对象
+    /*
+    add 定义规则 返回一个规则执行函数的数组
+    start 执行规则 返回一个错误信息的结果
+    都将具体的逻辑委托给了strategies 对象
+    符合了单一职责和开放封闭原则
+    开放算法的扩展
+    封闭了Validator的修改
+    */
 var Validator = function() {
-    this.cache = [];
-  }
-  // 为了做验证拼参数
+        this.cache = [];
+    }
+    // 为了做验证拼参数
 Validator.prototype.add = function(dom, rules) {
-    for (var i = 0, rule; rule = rules[i++];) {
-      // 闭包利用的非常合理 把每个规则都封装到闭包里面真是神来之笔
-      // 验证的函数是相对于规则来说的 其中公有的部分是对应的dom
-      (function(rule) {
-        // 私有的部分是规则内部的解析参数
-        var strategArr = rule.stratege.split(":");
-        var errMsg = rule.errMsg;
-        var result;
-        var ifInit = true;
-        var stra;
-        // 将验证的函数放到cache里面
-        this.cache.push(function() {
-          // 组装参数列表 这里的function都是闭包 里面的变量都不会被销毁
-          if (ifInit) {
-            stra = strategArr.shift();
-            strategArr.push(errMsg);
-            ifInit = false;
-          }
-          strategArr.unshift(dom.value);
-          // console.log(stra, strategArr)
-          return result = strategies[stra].apply(dom, strategArr);
+        for (var i = 0, rule; rule = rules[i++];) {
+            // 闭包利用的非常合理 把每个规则都封装到闭包里面真是神来之笔
+            // 验证的函数是相对于规则来说的 其中公有的部分是对应的dom
+            (function(rule) {
+                // 私有的部分是规则内部的解析参数
+                var strategArr = rule.stratege.split(":");
+                var errMsg = rule.errMsg;
+                var result;
+                var ifInit = true;
+                var stra;
+                // 将验证的函数放到cache里面
+                this.cache.push(function() {
+                    // 组装参数列表 这里的function都是闭包 里面的变量都不会被销毁
+                    if (ifInit) {
+                        stra = strategArr.shift();
+                        strategArr.push(errMsg);
+                        ifInit = false;
+                    }
+                    strategArr.unshift(dom.value);
+                    // console.log(stra, strategArr)
+                    return result = strategies[stra].apply(dom, strategArr);
 
-        }.after(function(result) {
-          console.log(errMsg, result)
-          if (result == undefined) {
-            dom.nextSibling.innerHTML = "";
-          } else {
-            dom.nextSibling.innerHTML = result;
-          }
-          strategArr.shift()
-        }))
-      }.bind(this))(rule)
+                }.after(function(result) {
+                    console.log(errMsg, result)
+                    if (result == undefined) {
+                        dom.nextSibling.innerHTML = "";
+                    } else {
+                        dom.nextSibling.innerHTML = result;
+                    }
+                    strategArr.shift()
+                }))
+            }.bind(this))(rule)
+        }
     }
-  }
-  // 验证开始
+    // 验证开始
 Validator.prototype.start = function() {
-    var ifValidate = false;
-    for (var j = 0, validaFuncc; validaFuncc = this.cache[j++];) {
-      var err = validaFuncc();
-      // console.log(err)
-      if (err) {
-        // ifValidate = true;
-        return err;
-      }
+        var ifValidate = false;
+        for (var j = 0, validaFuncc; validaFuncc = this.cache[j++];) {
+            var err = validaFuncc();
+            // console.log(err)
+            if (err) {
+                // ifValidate = true;
+                return err;
+            }
+        }
+        // return ifValidate;
     }
-    // return ifValidate;
-  }
-  // 客户端调用
+    // 客户端调用
 var registerForm = document.getElementById('registerForm');
 var validator = new Validator();
 var userName = document.getElementById('userName');
@@ -110,16 +110,16 @@ var submit = document.getElementById('submit');
 //   errMsg: '用户名长度不能小于10 位'
 // }]);
 validator.add(userName, [{
-  stratege: 'isNonEmpty',
-  errMsg: '用户名不能为空'
+    stratege: 'isNonEmpty',
+    errMsg: '用户名不能为空'
 }]);
 
 validator.add(phone, [{
-  stratege: 'isNonEmpty',
-  errMsg: '手机号不能为空'
+    stratege: 'isNonEmpty',
+    errMsg: '手机号不能为空'
 }, {
-  stratege: 'length:11',
-  errMsg: '手机号为11位'
+    stratege: 'length:11',
+    errMsg: '手机号为11位'
 }]);
 
 
@@ -133,14 +133,29 @@ validator.add(phone, [{
 // }]);
 
 var validataFunc = function() {
-  var errorMsg = validator.start();
-  return errorMsg;
+    var errorMsg = validator.start();
+    return errorMsg;
 }
 submit.onclick = function() {
-  var errorMsg = validataFunc();
-  if (errorMsg) {
-    return false;
-  }
+    var errorMsg = validataFunc();
+    if (errorMsg) {
+        return false;
+    } else {
+        reqwest({
+            url: '/order/signup',
+            method: 'post',
+            data: {
+                "kind_id": [1,2],
+                "purchase_id": "test",
+                "phone": "1342461052",
+                "name": "aaa",
+                "wechat_no": "asdad"
+            },
+            success: function(resp) {
+                console.log("请求成功")
+            }
+        })
+    }
 };
 // 策略模式
 // 组合 不同类之间的组合
